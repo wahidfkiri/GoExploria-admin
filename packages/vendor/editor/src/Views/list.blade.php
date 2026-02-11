094d362cccb1912a266fa2e151257bd659d24132
wahid fkiri <wahidfkiri5@gmail.com>
2026-02-11 15:56:57 +0100

    update changes

--- packages/vendor/editor/src/Views/list.blade.php
+++ packages/vendor/editor/src/Views/list.blade.php
@@ -353,6 +353,8 @@
     
     <!-- Bootstrap JS Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- jQuery (for AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         // Configuration
         let currentPage = 1;
