2309b3c1db74fab510a562f500714b44a6c7f19f
wahid fkiri <wahidfkiri5@gmail.com>
2026-03-06 14:56:35 +0100

    update module tasks

--- packages/vendor/project/src/Controllers/ProjectController.php
+++ packages/vendor/project/src/Controllers/ProjectController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
 
 class ProjectController extends Controller
 {
