250b35ffbf3c14bca6f1df875816b7995c6c0b75
wahid fkiri <wahidfkiri5@gmail.com>
2026-04-07 15:53:52 +0200

    fix title

--- packages/vendor/cms/src/Controllers/Web/WebThemeController.php
+++ packages/vendor/cms/src/Controllers/Web/WebThemeController.php
@@ -64,6 +64,7 @@ class WebThemeController extends Controller
         } 
         // Vérifier la session
         elseif (session()->has('theme_preview_mode') && session('theme_preview_mode') === true) {
+            return null;
             $this->previewMode = true;
             $previewThemeId = session('preview_theme_id');
             if ($previewThemeId) {
@@ -80,7 +81,7 @@ class WebThemeController extends Controller
             }
         } 
         else {
-          return  $theme = $this->getThemeToUse();
+            $theme = $this->getThemeToUse();
         }
         
         if (!$theme) {
